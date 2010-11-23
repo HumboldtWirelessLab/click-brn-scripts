1b48bf1bea42254a468f3415c73521ff8cf6b764
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2010-11-23 13:00:42 +0100

    update

--- 004-dsr_simpleflow/dsr_simpleflow.ctl
+++ 004-dsr_simpleflow/dsr_simpleflow.ctl
@@ -1,4 +1,4 @@
 #TIME	NODE(S)	DEVICE	MODE	ELEMENT	HANDLER		VALUE
-100	sk1 	ath0	write	sf	add_flow	sk1:eth sk14:eth 1000 100 0 100 1
+100	sk1 	ath0	write	sf	add_flow	sk1:eth sk14:eth 1000 100 0 100 true
 119	sk1	ath0	read	sf	stats
 119	sk14	ath0	read	sf	stats
--- 004-dsr_simpleflow/flow_setup.sh
+++ 004-dsr_simpleflow/flow_setup.sh
@@ -7,10 +7,10 @@ TIME=$2
 for n1 in $NODES; do
   for n2 in $NODES; do
     if [ "$n1" != "$n2" ]; then
-      echo "$TIME $n1 ath0 write sf add_flow $n1:eth $n2:eth 1000 100 0 100 1"
+      echo "$TIME $n1 ath0 write sf add_flow $n1:eth $n2:eth 1000 100 0 100 true"
       TIME=`expr $TIME + 5`
       echo "$TIME $n1 ath0 read  sf stats"
-      echo "$TIME $n1 ath0 write sf add_flow $n1:eth $n2:eth 1000 100 0 100 0"
+      echo "$TIME $n1 ath0 write sf add_flow $n1:eth $n2:eth 1000 100 0 100 false"
       TIME=`expr $TIME + 3`
     fi
   done
