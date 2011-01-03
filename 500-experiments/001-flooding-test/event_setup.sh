f63053a224bdf224bd726a35a770c9ed24621e54
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-01-03 19:36:50 +0100

    FloodingExp: event_setup.sh update

--- 500-experiments/001-flooding-test/event_setup.sh
+++ 500-experiments/001-flooding-test/event_setup.sh
@@ -4,6 +4,7 @@ NODES=`cat $1 | grep -v "#" | uniq`
 
 TIME=$2
 
+RESET_CHANNEL=6
 
 if [ "x$MODE" = "x" ]; then
   MODE=EVENT_LINKTABLE
@@ -22,17 +23,18 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
   for n1 in $NODES; do
       echo "$TIME $n1 ath0 write device_wifi/data_suppressor active1 false"
       echo "$TIME $n1 ath0 write device_wifi/data_suppressor active0 false"
+      echo "$TIME $n1 ath0 write device_wifi/linkprobe_suppressor active0 false"
 
       echo "$TIME $n1 ath0 write eh reset true"
       echo "$TIME $n1 ath0 write event_notifier reset true"
       echo "$TIME $n1 ath0 write flooding/fl reset true"
       echo "$TIME $n1 ath0 write device_wifi/data_queue reset true"
 
-#     echo "$TIME $n1 ath0 write device_wifi/wifidevice/ath_op channel 13"
-      echo "$TIME $n1 ath0 write device_wifi/wifidevice/sc set_channel ath0 14"
+#     echo "$TIME $n1 ath0 write device_wifi/wifidevice/ath_op channel $RESET_CHANNEL"
+      echo "$TIME $n1 ath0 write device_wifi/wifidevice/sc set_channel ath0 $RESET_CHANNEL"
   done
 
-  TIME=`expr $TIME + 30`
+  TIME=`expr $TIME + 10`
 
   for n1 in $NODES; do
     if [ "x$CHANNEL" != "x" ]; then
@@ -48,9 +50,12 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
 
   done
 
+  TIME=`expr $TIME + 10`
+
   for n1 in $NODES; do
       echo "$TIME $n1 ath0 write device_wifi/data_suppressor active1 true"
       echo "$TIME $n1 ath0 write device_wifi/data_suppressor active0 true"
+      echo "$TIME $n1 ath0 write device_wifi/linkprobe_suppressor active0 true"
   done
 
   if [ "x$CHANNEL" != "x" ]; then
@@ -59,7 +64,16 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
     TIME=`expr $TIME + 5`
   fi
 
+##############################################################################################
+#################################### FOR EACH NODE ###########################################
+##############################################################################################
+
   for n1 in $NODES; do
+
+    ################################################################################
+    ################################## MEASUREMENT #################################
+    ################################################################################
+
     for n2 in $NODES; do
       echo "$TIME $n2 ath0 read device_wifi/link_stat bcast_stats flooding_stats.$n1"
     done
@@ -73,38 +87,41 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       TIME=`expr $TIME + 1`
     done
 
-    TIME=`expr $TIME + 30`
+    for n2 in $NODES; do
+      echo "$TIME $n2 ath0 read device_wifi/wifidevice/cst stats_xml flooding_stats.$n1"
+      echo "$TIME $n2 ath0 read sys_info systeminfo flooding_stats.$n1"
+    done
+
+    TIME=`expr $TIME + 10`
+
+    ################################################################################
+    ##################################### RESET ####################################
+    ################################################################################
 
     for n2 in $NODES; do
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active1 false"
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active0 false"
+      echo "$TIME $n2 ath0 write device_wifi/linkprobe_suppressor active0 false"
       echo "$TIME $n2 ath0 write device_wifi/data_queue reset true"
-#      echo "$TIME $n2 ath0 write device_wifi/wifidevice/ath_op channel 13"
-      echo "$TIME $n2 ath0 write device_wifi/wifidevice/sc set_channel ath0 14"
+
+#     echo "$TIME $n2 ath0 write device_wifi/wifidevice/ath_op channel $RESET_CHANNEL"
+      echo "$TIME $n2 ath0 write device_wifi/wifidevice/sc set_channel ath0 $RESET_CHANNEL"
+
+      echo "$TIME $n2 ath0 read device_wifi/wifidevice/ath_op pkt_count /dev/null"
     done
 
-    TIME=`expr $TIME + 30`
+    TIME=`expr $TIME + 10`
 
     for n2 in $NODES; do
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active0 true"
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active1 true"
     done
 
-    TIME=`expr $TIME + 30`
+    TIME=`expr $TIME + 10`
 
     for n2 in $NODES; do
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active0 false"
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active1 false"
-    done
-
-    for n2 in $NODES; do
-        echo "$TIME $n2 ath0 read eh stats flooding_stats.$n1"
-        echo "$TIME $n2 ath0 write eh reset true"
-        echo "$TIME $n2 ath0 write event_notifier reset true"
-        echo "$TIME $n2 ath0 read flooding/fl stats flooding_stats.$n1"
-        echo "$TIME $n2 ath0 write flooding/fl reset true"
-        echo "$TIME $n2 ath0 read sys_info systeminfo flooding_stats.$n1"
-        echo "$TIME $n2 ath0 read device_wifi/wifidevice/cst stats_xml flooding_stats.$n1"
     done
 
     for n2 in $NODES; do
@@ -112,12 +129,32 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       echo "$TIME $n2 ath0 write device_wifi/wifidevice/sc set_channel ath0 $CHANNEL"
     done
 
+    ################################################################################
+    ################################ READ STATS ####################################
+    ################################################################################
+
+    for n2 in $NODES; do
+        echo "$TIME $n2 ath0 read eh stats flooding_stats.$n1"
+        echo "$TIME $n2 ath0 write eh reset true"
+        echo "$TIME $n2 ath0 write event_notifier reset true"
+        echo "$TIME $n2 ath0 read flooding/fl stats flooding_stats.$n1"
+        echo "$TIME $n2 ath0 write flooding/fl reset true"
+        echo "$TIME $n1 ath0 read device_wifi/wifidevice/ath_op pkt_count packet_stats.$n1"
+    done
+
     for n2 in $NODES; do
       echo "$TIME $n2 ath0 write device_wifi/data_queue reset true"
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active0 true"
       echo "$TIME $n2 ath0 write device_wifi/data_suppressor active1 true"
+      echo "$TIME $n2 ath0 write device_wifi/linkprobe_suppressor active0 true"
     done
+
     TIME=`expr $TIME + 10`
+
+    ################################################################################
+    ################################# END RESET ####################################
+    ################################################################################
+
   done
 
 fi
