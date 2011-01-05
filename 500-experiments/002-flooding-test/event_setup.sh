7ba51519be0fced9fd05f6f0b7c7fa62b9b0fb35
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-01-05 18:54:10 +0100

    update

--- 500-experiments/002-flooding-test/event_setup.sh
+++ 500-experiments/002-flooding-test/event_setup.sh
@@ -31,11 +31,7 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       echo "$TIME $n1 ath0 write device_wifi/data_queue reset true"
 
       echo "$TIME $n1 ath0 write device_wifi/wifidevice/ath_op clear_hw_queues wifi0"
-#     echo "$TIME $n1 ath0 write device_wifi/wifidevice/ath_op channel $RESET_CHANNEL"
-#     echo "$TIME $n1 ath0 write device_wifi/wifidevice/sc set_channel ath0 $RESET_CHANNEL"
   done
-
-#  TIME=`expr $TIME + 10`
 
   for n1 in $NODES; do
     if [ "x$CHANNEL" != "x" ]; then
@@ -50,8 +46,6 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
     fi
 
   done
-
-#  TIME=`expr $TIME + 10`
 
   for n1 in $NODES; do
       echo "$TIME $n1 ath0 write device_wifi/data_suppressor active1 true"
@@ -79,9 +73,7 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       echo "$TIME $n2 ath0 read device_wifi/link_stat bcast_stats flooding_stats.$n1"
     done
 
-    TIME=`expr $TIME + 1`
     echo "$TIME $n1 ath0 write event_notifier payload_size $PACKETSIZE"
-    TIME=`expr $TIME + 1`
 
     for s in `seq 1 $PACKETS`; do
       echo "$TIME $n1 ath0 write event_notifier event now"
@@ -93,7 +85,7 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       echo "$TIME $n2 ath0 read sys_info systeminfo flooding_stats.$n1"
     done
 
-    TIME=`expr $TIME + 10`
+    TIME=`expr $TIME + 2`
 
     ################################################################################
     ##################################### RESET ####################################
@@ -105,31 +97,10 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       echo "$TIME $n2 ath0 write device_wifi/linkprobe_suppressor active0 false"
       echo "$TIME $n2 ath0 write device_wifi/data_queue reset true"
 
-      echo "$TIME $n1 ath0 write device_wifi/wifidevice/ath_op clear_hw_queues wifi0"
-#     echo "$TIME $n2 ath0 write device_wifi/wifidevice/ath_op channel $RESET_CHANNEL"
-#     echo "$TIME $n2 ath0 write device_wifi/wifidevice/sc set_channel ath0 $RESET_CHANNEL"
+      echo "$TIME $n2 ath0 write device_wifi/wifidevice/ath_op clear_hw_queues wifi0"
 
       echo "$TIME $n2 ath0 read device_wifi/wifidevice/ath_op pkt_count /dev/null"
     done
-
-#    TIME=`expr $TIME + 10`
-
-#    for n2 in $NODES; do
-#      echo "$TIME $n2 ath0 write device_wifi/data_suppressor active0 true"
-#      echo "$TIME $n2 ath0 write device_wifi/data_suppressor active1 true"
-#    done
-
-#    TIME=`expr $TIME + 10`
-
-#    for n2 in $NODES; do
-#      echo "$TIME $n2 ath0 write device_wifi/data_suppressor active0 false"
-#      echo "$TIME $n2 ath0 write device_wifi/data_suppressor active1 false"
-#    done
-
-#    for n2 in $NODES; do
-#     echo "$TIME $n2 ath0 write device_wifi/wifidevice/ath_op channel $CHANNEL"
-#      echo "$TIME $n2 ath0 write device_wifi/wifidevice/sc set_channel ath0 $CHANNEL"
-#    done
 
     ################################################################################
     ################################ READ STATS ####################################
@@ -141,7 +112,7 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
         echo "$TIME $n2 ath0 write event_notifier reset true"
         echo "$TIME $n2 ath0 read flooding/fl stats flooding_stats.$n1"
         echo "$TIME $n2 ath0 write flooding/fl reset true"
-        echo "$TIME $n1 ath0 read device_wifi/wifidevice/ath_op pkt_count packet_stats.$n1"
+        echo "$TIME $n2 ath0 read device_wifi/wifidevice/ath_op pkt_count packet_stats.$n1"
     done
 
     for n2 in $NODES; do
@@ -151,7 +122,7 @@ if [ "$MODE" = "EVENT_LINKTABLE" ]; then
       echo "$TIME $n2 ath0 write device_wifi/linkprobe_suppressor active0 true"
     done
 
-    TIME=`expr $TIME + 10`
+    TIME=`expr $TIME + 2`
 
     ################################################################################
     ################################# END RESET ####################################
