5fcb6baa6d4fae7a5552f3c0fcf25819b0c5d461
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-05-09 12:42:38 +0200

    remove doubled stuff

--- 503-click/001-pcap_performance/evaluation/system/sysinfo_mat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in `seq 1 10`; do
-./grabsysinfo.pl 2>/dev/null | xsltproc systomat.xslt -
-#cat ./data.txt | xsltproc systomat.xslt -
-#./grabsysinfo.pl > ./data.txt
-sleep 60
-done
