d408c9e641e93cb7bcc125313fe9b79435cb656e
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-05-16 00:32:20 +0200

    80211n cap: add eval

---
+++ 500-experiments/006-80211n-measurements/002-80211n-capabilities/evaluation/eval.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+        DIR=$dir
+	;;
+  ".")
+        DIR=$pwd/$dir
+        ;;
+    *)
+        echo "Error while getting directory"
+        exit -1
+        ;;
+esac
+
+if [ "x$RESULTDIR" = "x" ]; then
+  if [ "x$1" = "x" ]; then
+    RESULTDIR=$DIR/../
+  else
+    RESULTDIR=$1
+    SIGN=`echo $RESULTDIR | cut -b 1`
+
+    case "$SIGN" in
+      "/")
+         ;;
+       *)
+         RESULTDIR="$PWD/$RESULTDIR"
+         ;;
+    esac
+  fi
+fi
+
+DIRNUM=1
+
+echo "RATEINDEX BANDWIDTH SGI GF FEC SHORTPREAMBLE STBC CHANNEL RX_ALL RX_OK RX_ERR" > $RESULTDIR/result.txt 
+
+while [ -e $RESULTDIR/$DIRNUM ]; do
+
+  if [ -f $RESULTDIR/$DIRNUM/params ]; then
+    ( cd $RESULTDIR/$DIRNUM; WIFI=802 fromdump.sh wndr231.wlan0.raw.dump > dump_raw.out )
+    RX_NUM=`cat $RESULTDIR/$DIRNUM/dump_raw.out | grep -e ":[[:space:]]*132[[:space:]]|" | egrep "13.0Mb|14.4Mb|27.0Mb|30.0Mb|65.0Mb|72.2Mb|135.0Mb|150.0Mb|130.0Mb|144.4Mb|270.0Mb|300.0Mb" | wc -l`
+    RX_OK=`cat $RESULTDIR/$DIRNUM/dump_raw.out | grep -e ":[[:space:]]*132[[:space:]]|" | egrep "13.0Mb|14.4Mb|27.0Mb|30.0Mb|65.0Mb|72.2Mb|135.0Mb|150.0Mb|130.0Mb|144.4Mb|270.0Mb|300.0Mb" | grep OKPacket | grep -v err | wc -l`
+    RX_ERR=`cat $RESULTDIR/$DIRNUM/dump_raw.out | grep -e ":[[:space:]]*132[[:space:]]|" | egrep "13.0Mb|14.4Mb|27.0Mb|30.0Mb|65.0Mb|72.2Mb|135.0Mb|150.0Mb|130.0Mb|144.4Mb|270.0Mb|300.0Mb" | grep err | wc -l`
+
+    . $RESULTDIR/$DIRNUM/params
+  
+    echo "$PARAMS_RATEINDEX $PARAMS_SGI $PARAMS_GF $PARAMS_FEC $PARAMS_SHORTPREAMBLE $PARAMS_STBC $PARAMS_CHANNEL $RX_NUM $RX_OK $RX_ERR" >> $RESULTDIR/result.txt
+  fi
+
+  DIRNUM=`expr $DIRNUM + 1`
+done
+
+exit 0
