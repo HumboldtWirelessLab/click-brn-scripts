e1f271a4320a7f610f887d6911b50351f85f4e9e
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2011-05-17 18:09:45 +0200

    Update 80211n measurements

--- 500-experiments/006-80211n-measurements/002-80211n-capabilities/evaluation/eval.sh
+++ 500-experiments/006-80211n-measurements/002-80211n-capabilities/evaluation/eval.sh
@@ -37,19 +37,25 @@ fi
 
 DIRNUM=1
 
-echo "RATEINDEX BANDWIDTH SGI GF FEC SHORTPREAMBLE STBC CHANNEL RX_ALL RX_OK RX_ERR" > $RESULTDIR/result.txt 
+echo "INDEX RATEINDEX BANDWIDTH SGI GF FEC SHORTPREAMBLE STBC CHANNEL RX_ALL RX_OK RX_ERR" > $RESULTDIR/result.txt 
 
 while [ -e $RESULTDIR/$DIRNUM ]; do
 
   if [ -f $RESULTDIR/$DIRNUM/params ]; then
-    ( cd $RESULTDIR/$DIRNUM; WIFI=802 fromdump.sh wndr231.wlan0.raw.dump > dump_raw.out )
+    if [ ! -f $RESULTDIR/$DIRNUM/dump_raw.out ]; then
+      if [ -f $RESULTDIR/$DIRNUM/wndr231.wlan0.raw.dump ]; then
+        ( cd $RESULTDIR/$DIRNUM; WIFI=802 fromdump.sh wndr231.wlan0.raw.dump > dump_raw.out )
+      else
+        ( cd $RESULTDIR/$DIRNUM; WIFI=802 fromdump.sh wndr231.wlan1.raw.dump > dump_raw.out )
+      fi
+    fi
     RX_NUM=`cat $RESULTDIR/$DIRNUM/dump_raw.out | grep -e ":[[:space:]]*132[[:space:]]|" | egrep "13.0Mb|14.4Mb|27.0Mb|30.0Mb|65.0Mb|72.2Mb|135.0Mb|150.0Mb|130.0Mb|144.4Mb|270.0Mb|300.0Mb" | wc -l`
     RX_OK=`cat $RESULTDIR/$DIRNUM/dump_raw.out | grep -e ":[[:space:]]*132[[:space:]]|" | egrep "13.0Mb|14.4Mb|27.0Mb|30.0Mb|65.0Mb|72.2Mb|135.0Mb|150.0Mb|130.0Mb|144.4Mb|270.0Mb|300.0Mb" | grep OKPacket | grep -v err | wc -l`
     RX_ERR=`cat $RESULTDIR/$DIRNUM/dump_raw.out | grep -e ":[[:space:]]*132[[:space:]]|" | egrep "13.0Mb|14.4Mb|27.0Mb|30.0Mb|65.0Mb|72.2Mb|135.0Mb|150.0Mb|130.0Mb|144.4Mb|270.0Mb|300.0Mb" | grep err | wc -l`
 
     . $RESULTDIR/$DIRNUM/params
-  
-    echo "$PARAMS_RATEINDEX $PARAMS_SGI $PARAMS_GF $PARAMS_FEC $PARAMS_SHORTPREAMBLE $PARAMS_STBC $PARAMS_CHANNEL $RX_NUM $RX_OK $RX_ERR" >> $RESULTDIR/result.txt
+
+    echo "$DIRNUM $PARAMS_RATEINDEX $PARAMS_BANDWIDTH $PARAMS_SGI $PARAMS_GF $PARAMS_FEC $PARAMS_SHORTPREAMBLE $PARAMS_STBC $PARAMS_CHANNEL $RX_NUM $RX_OK $RX_ERR" >> $RESULTDIR/result.txt
   fi
 
   DIRNUM=`expr $DIRNUM + 1`
