19973ca5e931fbdb44d0e4b218e113bdbca755e9
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-08-21 16:58:54 +0200

    Update

--- 008-dart_simpleflow/evaluation/eval.sh
+++ 008-dart_simpleflow/evaluation/eval.sh
@@ -4,6 +4,15 @@ SEND_PACKETS=`cat $RESULTDIR/measurement.log | grep "<txflow src=" | sed 's#"# #
 ECHO_PACKETS=`cat $RESULTDIR/measurement.log | grep "<txflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $13}'`
 RECV_PACKETS=`cat $RESULTDIR/measurement.log | grep "<rxflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $9}'`
 
+
+if [ "x$ECHO_PACKETS" = "x" ]; then
+  ECHO_PACKETS=0
+fi
+
+if [ "x$RECV_PACKETS" = "x" ]; then
+  RECV_PACKETS=0
+fi
+
 echo -n "Send $SEND_PACKETS packet, received $ECHO_PACKETS packets and $RECV_PACKETS echo packets are received."
 
 if [ $ECHO_PACKETS -gt 0 ]; then
