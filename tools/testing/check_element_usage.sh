d953549530d698207d7c51b7ad71c91e92c119b7
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-10-10 17:20:51 +0200

    Add script

---
+++ tools/testing/check_element_usage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+GREPARG=`find $CLICKPATH/elements/brn2 -iname *.cc | xargs cat | grep EXPORT | sed "s#\(EXPORT_ELEMENT(\|)\|;\)##g" | tr '\n' '|'`
+GREPARG="\"$GREPARG-foo\""
+
+SEDARG=`find $CLICKPATH/elements/brn2 -iname *.cc | xargs cat | grep EXPORT | sed "s#\(EXPORT_ELEMENT(\|)\|;\)##g" | tr '\n' '|' | sed "s#|#\\\\\\\\|#g"`
+SEDARG="s#\($SEDARG-foo\)# \1 #g"
+
+#echo $SEDARG
+#echo $GREPARG
+
+if [ "x$FULLMEASUREMENT" != "x1" ]; then
+  FILES=$1
+else
+  FILES=`(cd $1; cat \`ls *.mes.*\` | grep -v "#" | awk '{print $7}' | grep -v "^-$")`
+  #echo $FILES
+fi
+
+(for i in $FILES; do cat $i; done)  | egrep $GREPARG | sed -e "$SEDARG" | tr ' ' '\n' | egrep $GREPARG | sort -u
