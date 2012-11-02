a17ff7ab0ecd756ab72eeacd47a79cb14242f39f
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-11-02 16:09:26 +0100

    Test.sh for measurement

--- 501-testbed/001-function_tests/test.sh
+++ 501-testbed/001-function_tests/test.sh
@@ -2,6 +2,30 @@
 
 rm -f testbed.pdf
 
-MODE=MEASUREMENT START=1 LIMIT=3 ../../tools/testing/gen_test_pdf.sh
+if [ "x$VALGRIND" = "x" ]; then
+  VALGRIND_PARAMS=0
+else
+  VALGRIND_PARAMS=$VALGRIND
+fi
+
+if [ "x$NOLATEX" = "x1" ] ; then
+    ENABLE_LATEX=0
+else
+    ENABLE_LATEX=1
+fi
+
+if [ "x$SIMULATOR" = "x" ]; then
+  SIMULATOR=ns
+fi
+
+if [ "x$LIMIT" = "x" ]; then
+  LIMIT=3
+fi
+
+if [ "x$START" = "x" ]; then
+  START=1
+fi
+
+LATEX=$ENABLE_LATEX VALGRIND=$VALGRIND_PARAMS MODE=MEASUREMENT START=$START LIMIT=$LIMIT ../../tools/testing/gen_test_pdf.sh
 
 exit 0
