a823feaa779fc124efecb8de47855bdb6b1d5220
Boris Schroeder <bschroed@informatik.hu-berlin.de>
2013-01-25 14:21:26 +0100

    alte Dateien löschen

--- 036-sender_tos/evaluation/scenario_hot_spot/func_convert_matrix_4D_2_3D.m
+++
@@ -1,4 +0,0 @@
-function [matrix_3d] = func_convert_matrix_4D_2_3D(matrix_4d,dim1)
-    matrix_3d = zeros(size(matrix_4d,2),size(matrix_4d,3),size(matrix_4d,4));
-    matrix_3d(:,:,:) = matrix_4d(dim1,:,:,:);
-end
