c3d88741aa0e1e08d02aabdbefb9b10d5171b215
Frank Lange <langefrq@informatik.hu-berlin.de>
2013-04-22 14:48:57 +0200

    BRN_REGMON Phantom Analyzer Click Element

---
+++ 500-experiments/015-regmon/004-analyze_phantom/evaluation/eval.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. $CONFIGFILE
+
+(cd $RESULTDIR;ATH=yes fromdump.sh foobar103.ath0.raw.dump | grep -v tx | awk '{print $2" "$30" "$53" "$9" "$11" "$33" "$41" "$45" "$49" "$51" "$73}' | grep -v ":" | sed "s#(RX)#0#g" > ./regmon_dump_info_foobar103 )
+(cd $RESULTDIR;regmon_hex2dec.sh regmon_data_foobar103)
+
+exit 0
+
