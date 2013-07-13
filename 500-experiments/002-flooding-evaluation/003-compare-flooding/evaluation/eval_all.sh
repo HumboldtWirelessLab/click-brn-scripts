05970eab8f42110e18deb1f3341e780f334ca96e
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-07-13 22:10:21 +0200

    Flooding: update

--- 500-experiments/002-flooding-evaluation/003-compare-flooding/evaluation/eval_all.sh
+++ 500-experiments/002-flooding-evaluation/003-compare-flooding/evaluation/eval_all.sh
@@ -16,6 +16,8 @@ for d in `(cd $RESULTDIR; ls -l | grep "^d" | grep -v "evaluation" | awk '{print
 
     ALGORITHMID=`echo $ALGORITHM | sed $ALGSEDARG`
     UNICAST_REJECTONEMPTYCS=`echo $UNICAST_REJECTONEMPTYCS | sed -e "s#true#1#g" | sed -e "s#false#0#g"`
+    BCAST2UNIC_FORCERESPONSIBILITY=`echo AST2UNIC_FORCERESPONSIBILITY | sed -e "s#true#1#g" | sed -e "s#false#0#g"`
+    BCAST2UNIC_USEASSIGNINFO=`echo $BCAST2UNIC_USEASSIGNINFO | sed -e "s#true#1#g" | sed -e "s#false#0#g"`
 
     INFO="$SIM $UNICASTSTRATEGY $PLACEMENT $UNICAST_PRESELECTION_STRATEGY $UNICAST_REJECTONEMPTYCS $UNICAST_UCASTPEERMETRIC $FLOODING_PASSIVE_ACK_RETRIES"
 
@@ -29,10 +31,11 @@ for d in `(cd $RESULTDIR; ls -l | grep "^d" | grep -v "evaluation" | awk '{print
       fi
     fi
 
-    cat $RESULTDIR/$d/evaluation/flooding_info/floodingstats.csv | sed "s#,# #g" | awk -v ALG=$ALGORITHMID -v N=$SIMID -v I="$INFO" -v E=$EXTRAINFO -v L=$UNICASTSTRATEGY -v C=$COLLISIONEN -v MR=$MACRETRIES -v MET=$FLOODING_MAXNBMETRIC -v PP=$FLOODING_LASTNODES_PP '{print N" "I" "ALG" "E" "L" "$3" "$10" "$2" "$1" "$7" "$9" "$6" "$5" "C" "MR" "MET" "PP }' >> result_flooding.dat
+    cat $RESULTDIR/$d/evaluation/flooding_info/floodingstats.csv | sed "s#,# #g" | awk -v ALG=$ALGORITHMID -v N=$SIMID -v I="$INFO" -v E=$EXTRAINFO -v L=$UNICASTSTRATEGY -v C=$COLLISIONEN -v MR=$MACRETRIES -v MET=$FLOODING_MAXNBMETRIC -v PP=$FLOODING_LASTNODES_PP -v FR=$BCAST2UNIC_FORCERESPONSIBILITY -v UA=$BCAST2UNIC_USEASSIGNINFO -v QD=$BCAST_RNDDELAYQUEUE_MAXDELAY -v S=$SEED '{print N" "I" "ALG" "E" "L" "$3" "$10" "$2" "$1" "$7" "$9" "$6" "$5" "C" "MR" "MET" "PP" "FR" "UA" "QD" "S }' >> result_flooding.dat
 
   fi
 
 done
 
 exit 0
+
