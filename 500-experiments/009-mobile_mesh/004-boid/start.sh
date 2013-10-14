bada44a1baa74ff6e5799f1ff08b047b3dc56023
Robert Fritz <rfritz@gmx.de>
2013-10-14 12:39:23 +0200

    update simulation

--- 500-experiments/009-mobile_mesh/004-boid/start.sh
+++ 500-experiments/009-mobile_mesh/004-boid/start.sh
@@ -6,34 +6,35 @@ dirname="";
 I=0;
 
 #initiale steuerungsfaktoren
-radius=30;
+radius=60;
 separation=10000;
 cohesion=-20;
 steerlimit=10000;
 gravitation=1000000;
 speed=4;
 
-probe_period=1000;
-probe_tau=100000;
+probe_period=200;
+probe_tau=4000;
 
-seedInit=1;
-seedPre=1;
-seedPost=1;
+gravitationXInit=400;
+gravitationYInit=400;
+gravitationX=400;
+gravitationY=400;
 
-gravitationInit=400;
-gravitationPre=400;
-gravitationPost=400;
+plmStartX=50;
+plmStartY=50;
 
 
 #stellschräubchen für die simulationen
 nodes=3;
 maxnodes=6;
-radIncrements=5;
+radIncrements=3;
 
 method="$1";
 nodeplm="cubic";
 
 fieldSize=750;
+fieldSizeInit=750;
 
 # check for existing folders
 if [ -e "1" ]; then rm -rf "1"; fi
@@ -44,13 +45,13 @@ if [ -e "$method" ]; then rm -rf "$method"; fi
 while [ "$nodes" -lt "$maxnodes" ]
 do
 	#initiale platzierung der knoten in der boid.plm generieren
-	placement.sh $nodes $nodeplm 50 50;
+	placement.sh $nodes $nodeplm $plmStartX $plmStartY;
 	
 	#laufvariable re-initialisieren
 	I=0;
 	
 	#simulationsparameter re-initialisieren
-	radius=30;
+	radius=60;
 	
 	
 	while [ "$I" -lt $radIncrements ]
@@ -77,8 +78,14 @@ do
 		
 		
 		dirname="$nodes""Knoten""$radius""Radius";
-
+		
+		sed -i s/"write boid.gravitation add $gravitationXInit $gravitationYInit 0 2000000,"/"write boid.gravitation add $gravitationX $gravitationY 0 2000000,"/g boid.click;
+		sed -i s/"FIELDSIZE=$fieldSizeInit"/"FIELDSIZE=$fieldSize"/g boid.des;
+		
 		run_sim.sh;
+		
+		sed -i s/"FIELDSIZE=$fieldSize"/"FIELDSIZE=$fieldSizeInit"/g boid.des;
+		sed -i s/"write boid.gravitation add $gravitationX $gravitationY 0 2000000,"/"write boid.gravitation add $gravitationXInit $gravitationYInit 0 2000000,"/g boid.click;
 		
 		if [ ! -e "$method" ]; then mkdir "$method"; fi
 		if [ -e 1 ]; then mv 1 $method/$dirname; fi
@@ -96,7 +103,7 @@ do
 
 		
 		#matlab scripte konfigurieren
-		sed -e s/"knoten = 9;"/"knoten = $(($nodes*$nodes));"/g -e s/"zeros(750,750);"/"zeros($fieldSize,$fieldSize);"/g MatLab/Coverage.m > $method/$dirname/MatLab/Coverage.m;
+		sed -e s/"gravitation = \[400,400\];"/"gravitation = \[$gravitationX,$gravitationY\];"/g -e s/"knoten = 9;"/"knoten = $(($nodes*$nodes));"/g -e s/"zeros($fieldSizeInit,$fieldSizeInit);"/"zeros($fieldSize,$fieldSize);"/g MatLab/Coverage.m > $method/$dirname/MatLab/Coverage.m;
 		sed -e s/"knoten = 9;"/"knoten = $(($nodes*$nodes));"/g MatLab/Distances.m > $method/$dirname/MatLab/Distances.m;
 		
 		#auswertung per matlab
@@ -115,6 +122,13 @@ do
 	done
 	
 	nodes=$(($nodes+1));
+	fieldSize=$(($fieldSize+250));
+	
+	gravitationX=$(($gravitationX+100));
+	gravitationY=$(($gravitationY+100));
+	
+	plmStartX=$(($plmStartX+25));
+	plmStartY=$(($plmStartY+25));
 	
 done
 
