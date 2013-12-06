5ef2f2a39558dc7835bcf95737d2bfaf119c00f8
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-12-06 20:23:27 +0100

    Flooding: update eval

--- 500-experiments/002-flooding-evaluation/003-compare-flooding/evaluation/floodstats_info.m
+++ 500-experiments/002-flooding-evaluation/003-compare-flooding/evaluation/floodstats_info.m
@@ -1,13 +1,13 @@
 %function floodstats_info(simple_filename, e2e_filename, prop_filename)
 
 if (~exist('basedir'))
-  basedir='/mnt/data/flooding/20131127/'
+  basedir=''
   %basedir='/mnt/data/flooding/20131130/'
 else
   basedir
 end
 
-load_data = 0
+load_data = 1
 
 if ( load_data == 1 )
   load(strcat(basedir,'flooding_pre_data.dat'),'-mat');
@@ -17,6 +17,9 @@ end
 simple_filename = strcat(basedir,'result_flooding_info.dat.simple');
 e2e_filename = strcat(basedir,'result_flooding_info.dat.e2e');
 prop_filename = strcat(basedir,'result_flooding_info.dat.unicast');
+mpr_filename = strcat(basedir,'result_flooding_info.dat.mpr');
+prob_filename = strcat(basedir,'result_flooding_info.dat.prob');
+
 
 NUM=1;
 
@@ -165,13 +168,172 @@ if (~exist('flood_sent'))
   flood_param_id = param_id;
 end
 
+%MPR
+if (~exist('mpr_sent'))
 
-%clear flood_e2e_sent;
+  param_id = flood_param_id;
+
+  data=load(mpr_filename);
+
+  % strategy
+  net_retries_naive = unique(data(data(:,UNICASTSTRATEGY) == 0 & data(:,E2E) == 0,FLOODING_NET_RETRIES));
+
+  net_retries_naive = [ 0 1 2 3 ]';
+
+  src_nodes = unique(data(:,SRCNODE));
+  params_value=unique(data(:,params),'rows');
+
+  %%
+  % NAIVE FLOODING
+  mpr_delays = [];
+  mpr_reach = [];
+  mpr_sent = [];
+  mpr_xlb = [];
+
+  for nn=1:size(net_retries_naive,1)
+    mpr_retries = net_retries_naive(nn);
+    method = data(data(:,UNICASTSTRATEGY) == 0 & data(:,FLOODING_NET_RETRIES) == mpr_retries & data(:,E2E) == 0,:);
+    seeds = unique(method(:,SEED));
+    flIds = unique(method(:,FLOODINGID));
+
+    all_delays = [];
+    reach = [];
+    all_sent = [];
+
+    mpr_xlb = [ mpr_xlb; [ 0 net_retries_naive(nn) 0 ]];
+
+    for ff=1:size(flIds,1)
+      for ss=1:size(seeds,1)
+        for sn=1:size(src_nodes,1)
+            fl_1 = method(method(:,FLOODINGID) == flIds(ff) & method(:,SEED) == seeds(ss) & (method(:,SRCNODE) == src_nodes(sn)),:);
+
+            %size(fl_1)
+            %fl_1(:,[RXNODE TXNODE SRCNODE ])
+            src_node = unique(fl_1(:,SRCNODE));
+            assert(size(src_node,1) == 1 & size(src_node,2) == 1);
+
+            start_time = min(fl_1(fl_1(:,RXNODE) == src_node,TIME));
+
+            rx_times = fl_1(fl_1(:,RXNODE) ~= src_node, [RXNODE TIME]);
+            %size(rx_times)
+            rx_nodes = unique(rx_times(:,1));
+            reach = [reach; size(rx_nodes,1)];
+
+            delays = rx_times(:,2) - start_time;
+            all_delays = [all_delays; delays];
+
+            fl_1_sent_src = unique(fl_1(fl_1(:,SRCNODE) == fl_1(:,RXNODE),NOSENT));
+            fl_1_sent_fwd = fl_1(fl_1(:,SRCNODE) == fl_1(:,TXNODE),NOSENT);
+
+            %size(fl_1_sent_src)
+            %size(fl_1_sent_fwd)
+
+            all_sent = [all_sent; sum(fl_1_sent_fwd)];
+        end
+      end
+    end
+
+    size(all_sent)
+
+    reach = reach + 1; % include src
+    reach = reach / size(unique(method(:,RXNODE)),1);
+    all_delays = all_delays * 1e3;
+
+    mpr_delays = [mpr_delays; [all_delays zeros(size(all_delays,1),1) + param_id]];
+    mpr_reach = [mpr_reach; [reach zeros(size(reach,1),1) + param_id]];
+    mpr_sent = [mpr_sent; [all_sent zeros(size(all_sent,1),1) + param_id]];
+    param_id = param_id + 1;
+  end
+
+  mpr_param_id = param_id;
+end
+
+%%
+% PROB FLOODING
+if (~exist('prob_sent'))
+
+  param_id = mpr_param_id;
+
+  data=load(prob_filename);
+
+  prob_retries = unique(data(data(:,UNICASTSTRATEGY) == 0 & data(:,E2E) == 0, FLOODING_NET_RETRIES));
+  prob_retries = [ 0 1 2 3 ]';
+
+  prob_probs = unique(data(data(:,UNICASTSTRATEGY) == 0 & data(:,E2E) == 0, EXTRAINFO));
+  %prob_probs = [ 95 85 ]';
+
+  src_nodes = unique(data(:,SRCNODE));
+
+  prob_delays = [];
+  prob_reach = [];
+  prob_sent = [];
+  prob_xlb = [];
+
+  for pp=1:size(prob_probs,1)
+  for nn=1:size(prob_retries,1)
+    method = data(data(:,UNICASTSTRATEGY) == 0 & data(:,FLOODING_NET_RETRIES) == prob_retries(nn) & data(:,EXTRAINFO) == prob_probs(pp),:);
+    seeds = unique(method(:,SEED));
+    flIds = unique(method(:,FLOODINGID));
+
+    all_delays = [];
+    reach = [];
+    all_sent = [];
+
+    prob_xlb = [ prob_xlb; [ prob_probs(pp) 0 prob_retries(nn) ]];
+
+    for ff=1:size(flIds,1)
+      for ss=1:size(seeds,1)
+        for sn=1:size(src_nodes,1)
+            fl_1 = method(method(:,FLOODINGID) == flIds(ff) & method(:,SEED) == seeds(ss) & (method(:,SRCNODE) == src_nodes(sn)),:);
+
+            %size(fl_1)
+            %fl_1(:,[RXNODE TXNODE SRCNODE ])
+            src_node = unique(fl_1(:,SRCNODE));
+            assert(size(src_node,1) == 1 & size(src_node,2) == 1);
+
+            start_time = min(fl_1(fl_1(:,RXNODE) == src_node,TIME));
+
+            rx_times = fl_1(fl_1(:,RXNODE) ~= src_node, [RXNODE TIME]);
+            %size(rx_times)
+            rx_nodes = unique(rx_times(:,1));
+            reach = [reach; size(rx_nodes,1)];
+
+            delays = rx_times(:,2) - start_time;
+            all_delays = [all_delays; delays];
+
+            fl_1_sent_src = unique(fl_1(fl_1(:,SRCNODE) == fl_1(:,RXNODE),NOSENT));
+            fl_1_sent_fwd = fl_1(fl_1(:,SRCNODE) == fl_1(:,TXNODE),NOSENT);
+
+            %size(fl_1_sent_src)
+            %size(fl_1_sent_fwd)
+
+            all_sent = [all_sent; sum(fl_1_sent_fwd)];
+        end
+      end
+    end
+
+    size(all_sent)
+
+    reach = reach + 1; % include src
+    reach = reach / size(unique(method(:,RXNODE)),1);
+    all_delays = all_delays * 1e3;
+
+    prob_delays = [prob_delays; [all_delays zeros(size(all_delays,1),1) + param_id]];
+    prob_reach = [prob_reach; [reach zeros(size(reach,1),1) + param_id]];
+    prob_sent = [prob_sent; [all_sent zeros(size(all_sent,1),1) + param_id]];
+    param_id = param_id + 1;
+  end
+  end
+
+  prob_param_id = param_id;
+end
+
+
 %%
 % E2E FLOODING
 if (~exist('flood_e2e_sent'))
 
-  param_id = flood_param_id;
+  param_id = prob_param_id;
 
   data=load(e2e_filename);
 
@@ -321,7 +483,11 @@ if (~exist('prop_sent'))
         param_id = param_id + 1;
     end
   end
+
+  prop_param_id = param_id;
+
 end
+
 
 if (~exist('nonodes'))
   nonodes = length(unique(fl_1(:,RXNODE)));
@@ -329,7 +495,7 @@ if (~exist('nonodes'))
 end
 
 if ( load_data == 0 )
-  save('flooding_pre_data.dat','flood_xlb', 'e2e_xlb', 'prop_xlb', 'flood_delays', 'flood_e2e_delays', 'prop_delays', 'flood_reach', 'flood_e2e_reach', 'prop_reach', 'flood_sent', 'flood_e2e_sent','prop_sent', 'nonodes');
+  save('flooding_pre_data.dat','flood_xlb', 'e2e_xlb', 'prop_xlb', 'mpr_xlb', 'prob_xlb', 'flood_delays', 'flood_e2e_delays', 'prop_delays', 'mpr_delays', 'prob_delays', 'flood_reach', 'flood_e2e_reach', 'prop_reach', 'mpr_reach', 'prob_reach','flood_sent', 'flood_e2e_sent','prop_sent', 'mpr_sent', 'prob_sent', 'nonodes');
 end
 
 %%
@@ -337,21 +503,21 @@ end
 %h1 = figure(1);
 h1=figure('Position', [100 100 700 500]);
 
-xlb=[ flood_xlb; e2e_xlb; prop_xlb];
+xlb=[ flood_xlb; mpr_xlb; prob_xlb; e2e_xlb; prop_xlb];
 xlbs = cell(size(xlb,1),1);
 for ii=1:size(xlb,1)
     xlbs{ii} = [int2str(xlb(ii,1)), '/', int2str(xlb(ii,2)), '/',int2str(xlb(ii,3)) ];
 end
 
-delays = [flood_delays; flood_e2e_delays; prop_delays];
-reach = [flood_reach; flood_e2e_reach; prop_reach];
-sent = [flood_sent; flood_e2e_sent; prop_sent];
+delays = [flood_delays; mpr_delays; prob_delays; flood_e2e_delays; prop_delays];
+reach = [flood_reach; mpr_reach; prob_reach; flood_e2e_reach; prop_reach];
+sent = [flood_sent; mpr_sent; prob_sent; flood_e2e_sent; prop_sent];
 
 efficiency = sent;
 efficiency(:,1) = efficiency(:,1)./(reach(:,1)*nonodes); % mac-tx per node, which recv the flood
 
 % filter
-idx = 1:21; %[1 4 8 9 10 11 15 16:27];
+idx = 1:33; %[1 4 8 9 10 11 15 16:27];
 
 d_med_val = zeros(1,size(idx,2));
 r_med_val = zeros(1,size(idx,2));
@@ -384,13 +550,31 @@ xlbs{2} ='1/0';
 xlbs{3} ='2/0';
 xlbs{4} ='3/0';
 xlbs{5} ='4/0';
+
+% MPR FL
+xlbs{6} ='0/0';
+xlbs{7} ='1/0';
+xlbs{8} ='2/0';
+xlbs{9} ='3/0';
+
+% MPR FL
+xlbs{10} ='0/0';
+xlbs{11} ='1/0';
+xlbs{12} ='2/0';
+xlbs{13} ='3/0';
+xlbs{14} ='0/0';
+xlbs{15} ='1/0';
+xlbs{16} ='2/0';
+xlbs{17} ='3/0';
+
 % E2E FL
-xlbs{6} ='0/1';
-xlbs{7} ='0/2';
-xlbs{8} ='0/3';
-xlbs{9} ='0/4';
+xlbs{18} ='0/1';
+xlbs{19} ='0/2';
+xlbs{20} ='0/3';
+xlbs{21} ='0/4';
+
 % Proposed FL
-for ii=10:24
+for ii=22:length(xlbs)
    xlbs{ii} = strrep(xlbs{ii}, '/0', ''); 
 end
 
@@ -438,8 +622,19 @@ annotation(h1,'textbox',...
     'LineStyle','none');
 
 % Create line
-annotation(h1,'line',[0.458571428571429 0.458571428571429],...
-    [0.91 0.022],'LineStyle','--');
+%annotation(h1,'line',[0.458571428571429 0.458571428571429],...
+%    [0.91 0.022],'LineStyle','--');
+
+line_hpos = [0.92 0.102];
+
+%naive vs mpr
+annotation(h1,'line',[0.248 0.248],line_hpos,'LineStyle','--');
+%mpr vs prob
+annotation(h1,'line',[0.342 0.342],line_hpos,'LineStyle','--');
+%prob vs e2e
+annotation(h1,'line',[0.529 0.529],line_hpos,'LineStyle','--');
+%e2e vs prop
+annotation(h1,'line',[0.624 0.624],line_hpos,'LineStyle','--');
 
 %%
 title('Delay');
@@ -498,8 +693,18 @@ annotation(h2,'textbox',...
     'LineStyle','none');
 
 % Create line
-annotation(h2,'line',[0.458571428571429 0.458571428571429],...
-    [0.91 0.022],'LineStyle','--');
+%annotation(h2,'line',[0.458571428571429 0.458571428571429],...
+%    [0.91 0.022],'LineStyle','--');
+
+
+%naive vs mpr
+annotation(h2,'line',[0.248 0.248],line_hpos,'LineStyle','--');
+%mpr vs prob
+annotation(h2,'line',[0.342 0.342],line_hpos,'LineStyle','--');
+%prob vs e2e
+annotation(h2,'line',[0.529 0.529],line_hpos,'LineStyle','--');
+%e2e vs prop
+annotation(h2,'line',[0.624 0.624],line_hpos,'LineStyle','--');
 
 %%
 title('Reachbility');
@@ -557,8 +762,17 @@ annotation(h3,'textbox',...
     'LineStyle','none');
 
 % Create line
-annotation(h3,'line',[0.458571428571429 0.458571428571429],...
-    [0.91 0.022],'LineStyle','--');
+%annotation(h3,'line',[0.458571428571429 0.458571428571429],...
+%    [0.91 0.022],'LineStyle','--');
+%naive vs mpr
+annotation(h3,'line',[0.248 0.248],line_hpos,'LineStyle','--');
+%mpr vs prob
+annotation(h3,'line',[0.342 0.342],line_hpos,'LineStyle','--');
+%prob vs e2e
+annotation(h3,'line',[0.529 0.529],line_hpos,'LineStyle','--');
+%e2e vs prop
+annotation(h3,'line',[0.624 0.624],line_hpos,'LineStyle','--');
+
 %%
 title('Efficiency');
 ylabel('Total number of MAC transmissions');
@@ -615,8 +829,18 @@ annotation(h4,'textbox',...
     'LineStyle','none');
 
 % Create line
-annotation(h4,'line',[0.458571428571429 0.458571428571429],...
-    [0.91 0.022],'LineStyle','--');
+%annotation(h4,'line',[0.458571428571429 0.458571428571429],...
+%    [0.91 0.022],'LineStyle','--');
+%naive vs mpr
+annotation(h4,'line',[0.248 0.248],line_hpos,'LineStyle','--');
+%mpr vs prob
+annotation(h4,'line',[0.342 0.342],line_hpos,'LineStyle','--');
+%prob vs e2e
+annotation(h4,'line',[0.529 0.529],line_hpos,'LineStyle','--');
+%e2e vs prop
+annotation(h4,'line',[0.624 0.624],line_hpos,'LineStyle','--');
+
+
 %%
 title('Efficiency');
 ylabel('Total number of MAC transmissions per reached node');
