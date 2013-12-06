5b7794a6c1186a1774a171f329a3eb8b09d9ef7a
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2013-12-06 13:28:26 +0100

    Update evaluationsname

--- 500-experiments/002-flooding-evaluation/003-compare-flooding/eval_again.sh
+++ 500-experiments/002-flooding-evaluation/003-compare-flooding/eval_again.sh
@@ -36,7 +36,7 @@ echo -n "" > $DIR/evaluation_finish
 
 for i in `ls -d *MBit*`; do
 
-  #ADDEVALUATION="evaluation/eval.sh flooding_info_2 flow_info evaluation/eval_post.sh"
+  #ADDEVALUATION="evaluation/eval.sh flooding_info flow_info evaluation/eval_post.sh"
   (cd $i/; PREEVALUATION="$PREEVALUATION" ADDEVALUATION="$ADDEVALUATION" /bin/bash ./eval_again.sh; echo $NUM >> $DIR/evaluation_finish ) &
 
   let NUM=NUM+1
