9014a923b1b87f337000700176e597e36eaa57eb
Robert Döring <rdoering.info@gmail.com>
2014-05-27 17:18:05 +0200

    Avoid eval error.

--- 065-rateselection_per_ann/evaluation/eval.sh
+++ 065-rateselection_per_ann/evaluation/eval.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Evaluation not implemented, yet."
+exit 2
+
 SEND_PACKETS=`cat $RESULTDIR/measurement.log | grep "<txflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $9}'`
 ECHO_PACKETS=`cat $RESULTDIR/measurement.log | grep "<txflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $13}'`
 RECV_PACKETS=`cat $RESULTDIR/measurement.log | grep "<rxflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $9}'`
