8e269c4c11e57c7be6ea5f01658c60f82d82978f
Robert Döring <rdoering.info@gmail.com>
2014-05-31 03:20:01 +0200

    Run xlstproc per eval.sh.

--- 065-rateselection_per_ann/002-multi_simpleflow/evaluation/eval.sh
+++ 065-rateselection_per_ann/002-multi_simpleflow/evaluation/eval.sh
@@ -1,25 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Evaluation not implemented, yet."
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+      DIR=$dir
+      ;;
+  ".")
+      DIR=$pwd/$dir
+      ;;
+    *)
+      echo "Error while getting directory"
+      exit -1
+      ;;
+esac
+
+xsltproc -o ${RESULTDIR}/measurement.postxslt ${DIR}/input_neurons.xslt ${RESULTDIR}/measurement.xml 
+
+echo "Mein Result: xsltproc returned $?"
+
 exit 2
 
-SEND_PACKETS=`cat $RESULTDIR/measurement.log | grep "<txflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $9}'`
-ECHO_PACKETS=`cat $RESULTDIR/measurement.log | grep "<txflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $13}'`
-RECV_PACKETS=`cat $RESULTDIR/measurement.log | grep "<rxflow src=" | sed 's#"# #g' | sed "s#=# #g" | awk '{print $9}'`
-
-if [ "x$ECHO_PACKETS" = "x" ]; then
-  ECHO_PACKETS=0
-fi
-if [ "x$RECV_PACKETS" = "x" ]; then
-  RECV_PACKETS=0
-fi
-
-echo -n "Send $SEND_PACKETS packet, received $RECV_PACKETS packets and $ECHO_PACKETS echo packets are received."
-
-if [ $ECHO_PACKETS -gt 10 ]; then
-  echo " OK !"
-  exit 0
-else
-  echo " Too bad !"
-  exit 2
-fi
