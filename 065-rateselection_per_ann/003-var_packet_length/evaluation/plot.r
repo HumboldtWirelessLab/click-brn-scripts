9ec574bb36e6909f2138380e7695402826ce1d30
Robert Döring <rdoering.info@gmail.com>
2014-06-02 09:48:57 +0200

    Use bar plot instead of line chart.

--- 065-rateselection_per_ann/003-var_packet_length/evaluation/plot.r
+++ 065-rateselection_per_ann/003-var_packet_length/evaluation/plot.r
@@ -4,13 +4,16 @@ file_name = "measurement.postxslt"
 dataset = read.csv(file_name, colClasses=c("character", "character", "integer", "integer"))
 id_list = unique(dataset$ID)
 for(id in id_list) {
-  dataset_of_link = dataset[dataset$ID==id,]  
+  dataset_of_link = dataset[dataset$ID==id,]
+  dataset_of_link$Rate = factor(dataset_of_link$Rate, levels=levels(factor(as.integer(dataset_of_link$Rate))))
   
-  plot = ggplot(data=dataset_of_link, aes(x=Length, y=Replies, group=Rate, color=Rate, fill=Rate))
+  plot = ggplot(data=dataset_of_link, aes(x=Length, y=Replies, group=Rate, color=Rate, fill=Rate, facet=Rate ))
   plot = plot + ggtitle(id)
   plot = plot + theme_bw()
-  plot = plot + geom_line()
-  plot = plot + geom_point()
+  #plot = plot + geom_line()
+  #plot = plot + geom_point()
+  plot = plot + geom_bar(position="dodge", stat="bin")
+  plot = plot + facet_wrap(~ Rate)
   plot 
   ggsave(file = paste('output-' , id , '.svg', sep=""))
 }
