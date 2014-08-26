5786b7ee28573a07655e893b5b2899b7e4ff98e6
Robert Döring <rdoering.info@gmail.com>
2014-08-26 21:13:58 +0200

    eval some common things, may failing, but its not important.

--- 500-experiments/016-topology/common_evaluation/eval.sh
+++ 500-experiments/016-topology/common_evaluation/eval.sh
@@ -32,12 +32,16 @@ $DIR/extract_nodes.py -p ${RESULTDIR}
 # Create runs.csv
 # Create bridges.csv
 #
+
 LINKS_EXTRACED_PATH=${RESULTDIR}/links_extract.xml
 ${DIR}/extract_dibadawn_links.py -f ${RESULTDIR}/measurement.xml > ${LINKS_EXTRACED_PATH}
 LINKS_RAW_PATH=${RESULTDIR}/links_raw.csv
 xsltproc -o ${LINKS_RAW_PATH} ${DIR}/dibadawn_links_to_csv.xslt ${LINKS_EXTRACED_PATH}
 LINKS_PATH=${RESULTDIR}/links_filtered.csv
 $DIR/filter_links.py -f ${LINKS_RAW_PATH} -e 100 > ${LINKS_PATH}
+DISTANCES_PATH=${RESULTDIR}/links_distances.csv
+$DIR/link_distances.py -n ${RESULTDIR}/nodes.csv -l ${LINKS_PATH} > ${DISTANCES_PATH}
+
 
 SEARCHES=${RESULTDIR}/searches.xml
 TOPO_PATH=${RESULTDIR}/topo_info.xml
@@ -52,12 +56,15 @@ xsltproc -o ${RESULTDIR}/bridges_unique_per_search.csv ${DIR}/dibadawn_unique_br
 COUNT_OF_RUNS=$(grep -e "^<DibadawnStartSearch" ${RESULTDIR}/measurement.xml | wc -l)
 echo -e "\"num_of_runs\"\n${COUNT_OF_RUNS}" > ${RESULTDIR}/runs.csv
 
-xsltproc -o ${RESULTDIR}/cycles.csv ${DIR}/dibadawn_asym_cycle_ration.xslt ${RESULTDIR}/measurement.xml
+grep -e "simpleflow>" -e "<CrossEdge" ${RESULTDIR}/measurement.xml > ${RESULTDIR}/crossedge_extract.xml
+xsltproc -o ${RESULTDIR}/cycles.csv ${DIR}/dibadawn_asym_cycle_ration.xslt ${RESULTDIR}/crossedge_extract.xml
  
 cd ${RESULTDIR}
 ../../common_evaluation/calc_articulation_points.R ${LINKS_PATH} > ${RESULTDIR}/theoretical_articulation_points.csv
 ../../common_evaluation/calc_bridges.R ${LINKS_PATH} > ${RESULTDIR}/theoretical_bridges.csv
 ../../common_evaluation/calc_f1_measure.R
+
+../../common_evaluation/plot_distances.R 
 
 exit 2
 
---
+++ 500-experiments/016-topology/common_evaluation/link_distances.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as Et
+import sys
+import csv
+import math
+from optparse import OptionParser
+
+
+def read_nodes_from_file(file_path):
+	xs = []
+	ys = []
+	zs = []
+	nodes = []
+	macs = []
+	with open(file_path, 'r') as f:
+		reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_ALL)
+		next(reader)
+		for row in reader:
+			xs.append(int(row[0]))
+			ys.append(int(row[1]))
+			zs.append(int(row[2]))
+			nodes.append(row[3])
+			macs.append(row[4])
+			
+	return xs, ys, zs, nodes, macs
+
+
+def get_node_dict(nodes):
+	result = {}
+
+	for x, y, z, name, mac in nodes:
+		result[mac] = ((x, y, z), name)
+
+	return result
+
+
+optParser = OptionParser()
+optParser.add_option("-l", "--link_file", dest="links_path", type="string", help="Path to Links XML file")
+optParser.add_option("-n", "--nodes_file", dest="nodes_path", type="string", help="Path to nodes CSV file")
+optParser.add_option("-e", "--metric_etx_limit", type="float", dest="etx_metric_limit", default=999999.9, help="ETX-metric limit for accepted links")
+(options, args) = optParser.parse_args()
+
+if not options.links_path or not options.nodes_path:
+	print("Failed: Missing input measurement file")
+	optParser.print_help()
+	sys.exit(-1)
+
+
+(xs, ys, zs, node_names, macs) = read_nodes_from_file(options.nodes_path)
+nodes = zip(xs, ys, zs, node_names, macs)
+node_dict = get_node_dict(nodes)
+
+used_content = list()
+num_filtered = 0
+num_used     = 0
+header 		 = str()
+print("\"node_a\",\"node_b\",\"distance\"")
+with open(options.links_path, 'r') as f:
+	reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_ALL)
+	next(reader)
+	
+	for row in reader:
+		a_node_mac = row[0]
+		b_node_mac = row[1]
+		etx_metric = row[2]
+
+		((a_x, a_y, a_z), a_name) = node_dict[a_node_mac]
+		((b_x, b_y, b_z), b_name) = node_dict[b_node_mac]
+
+		distance = math.sqrt(pow(a_x - b_x, 2) + pow(a_y - b_y, 2) + pow(a_z - b_z, 2))
+		print("{0},{1},{3},{2:.3f}".format(a_node_mac, b_node_mac,distance, etx_metric))
+
+
