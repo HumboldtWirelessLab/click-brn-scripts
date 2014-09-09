1fe851e58da9eea5748b8caa85f1dd54258d5bd6
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-09-09 17:35:03 +0200

    Flooding: compare update

--- 500-experiments/002-flooding-evaluation/003-compare-flooding/evaluation/floodstats_info.m
+++ 500-experiments/002-flooding-evaluation/003-compare-flooding/evaluation/floodstats_info.m
@@ -7,10 +7,18 @@ else
   basedir
 end
 
-load_data = 1
+load_data = 1;
+placement = 1; % 0 or 1
+suffix = '';
 
 if ( load_data == 1 )
-  load(strcat(basedir,'flooding_pre_data.dat'),'-mat');
+    if (placement == 0)
+        suffix = '_rnd100';
+        load(strcat(basedir,'flooding_pre_data_random.dat'),'-mat');
+    else
+        suffix = '_bb';
+        load(strcat(basedir,'flooding_pre_data_multibox.dat'),'-mat');
+    end
 end
 
 %ilename = 'result_flooding_info.dat';
@@ -168,91 +176,11 @@ if (~exist('flood_sent'))
   flood_param_id = param_id;
 end
 
-%MPR
-if (~exist('mpr_sent'))
-
-  param_id = flood_param_id;
-
-  data=load(mpr_filename);
-
-  % strategy
-  net_retries_naive = unique(data(data(:,UNICASTSTRATEGY) == 0 & data(:,E2E) == 0,FLOODING_NET_RETRIES));
-
-  net_retries_naive = [ 0 1 2 3 ]';
-
-  src_nodes = unique(data(:,SRCNODE));
-  params_value=unique(data(:,params),'rows');
-
-  %%
-  % NAIVE FLOODING
-  mpr_delays = [];
-  mpr_reach = [];
-  mpr_sent = [];
-  mpr_xlb = [];
-
-  for nn=1:size(net_retries_naive,1)
-    mpr_retries = net_retries_naive(nn);
-    method = data(data(:,UNICASTSTRATEGY) == 0 & data(:,FLOODING_NET_RETRIES) == mpr_retries & data(:,E2E) == 0,:);
-    seeds = unique(method(:,SEED));
-    flIds = unique(method(:,FLOODINGID));
-
-    all_delays = [];
-    reach = [];
-    all_sent = [];
-
-    mpr_xlb = [ mpr_xlb; [ 0 net_retries_naive(nn) 0 ]];
-
-    for ff=1:size(flIds,1)
-      for ss=1:size(seeds,1)
-        for sn=1:size(src_nodes,1)
-            fl_1 = method(method(:,FLOODINGID) == flIds(ff) & method(:,SEED) == seeds(ss) & (method(:,SRCNODE) == src_nodes(sn)),:);
-
-            %size(fl_1)
-            %fl_1(:,[RXNODE TXNODE SRCNODE ])
-            src_node = unique(fl_1(:,SRCNODE));
-            assert(size(src_node,1) == 1 & size(src_node,2) == 1);
-
-            start_time = min(fl_1(fl_1(:,RXNODE) == src_node,TIME));
-
-            rx_times = fl_1(fl_1(:,RXNODE) ~= src_node, [RXNODE TIME]);
-            %size(rx_times)
-            rx_nodes = unique(rx_times(:,1));
-            reach = [reach; size(rx_nodes,1)];
-
-            delays = rx_times(:,2) - start_time;
-            all_delays = [all_delays; delays];
-
-            fl_1_sent_src = unique(fl_1(fl_1(:,SRCNODE) == fl_1(:,RXNODE),NOSENT));
-            fl_1_sent_fwd = fl_1(fl_1(:,SRCNODE) == fl_1(:,TXNODE),NOSENT);
-
-            %size(fl_1_sent_src)
-            %size(fl_1_sent_fwd)
-
-            all_sent = [all_sent; sum(fl_1_sent_fwd)];
-        end
-      end
-    end
-
-    size(all_sent)
-
-    reach = reach + 1; % include src
-    reach = reach / size(unique(method(:,RXNODE)),1);
-    all_delays = all_delays * 1e3;
-
-    mpr_delays = [mpr_delays; [all_delays zeros(size(all_delays,1),1) + param_id]];
-    mpr_reach = [mpr_reach; [reach zeros(size(reach,1),1) + param_id]];
-    mpr_sent = [mpr_sent; [all_sent zeros(size(all_sent,1),1) + param_id]];
-    param_id = param_id + 1;
-  end
-
-  mpr_param_id = param_id;
-end
-
 %%
 % PROB FLOODING
 if (~exist('prob_sent'))
 
-  param_id = mpr_param_id;
+  param_id = flood_param_id;
 
   data=load(prob_filename);
 
@@ -260,6 +188,7 @@ if (~exist('prob_sent'))
   prob_retries = [ 0 1 2 3 ]';
 
   prob_probs = unique(data(data(:,UNICASTSTRATEGY) == 0 & data(:,E2E) == 0, EXTRAINFO));
+  prob_probs = sort(prob_probs,'descend');
   %prob_probs = [ 95 85 ]';
 
   src_nodes = unique(data(:,SRCNODE));
@@ -327,7 +256,6 @@ if (~exist('prob_sent'))
 
   prob_param_id = param_id;
 end
-
 
 %%
 % E2E FLOODING
@@ -408,11 +336,91 @@ if (~exist('flood_e2e_sent'))
   e2e_param_id = param_id;
 end
 
+%MPR
+if (~exist('mpr_sent'))
+
+  param_id = e2e_param_id;
+
+  data=load(mpr_filename);
+
+  % strategy
+  net_retries_naive = unique(data(data(:,UNICASTSTRATEGY) == 0 & data(:,E2E) == 0,FLOODING_NET_RETRIES));
+
+  net_retries_naive = [ 0 1 2 3 ]';
+
+  src_nodes = unique(data(:,SRCNODE));
+  params_value=unique(data(:,params),'rows');
+
+  %%
+  % NAIVE FLOODING
+  mpr_delays = [];
+  mpr_reach = [];
+  mpr_sent = [];
+  mpr_xlb = [];
+
+  for nn=1:size(net_retries_naive,1)
+    mpr_retries = net_retries_naive(nn);
+    method = data(data(:,UNICASTSTRATEGY) == 0 & data(:,FLOODING_NET_RETRIES) == mpr_retries & data(:,E2E) == 0,:);
+    seeds = unique(method(:,SEED));
+    flIds = unique(method(:,FLOODINGID));
+
+    all_delays = [];
+    reach = [];
+    all_sent = [];
+
+    mpr_xlb = [ mpr_xlb; [ 0 net_retries_naive(nn) 0 ]];
+
+    for ff=1:size(flIds,1)
+      for ss=1:size(seeds,1)
+        for sn=1:size(src_nodes,1)
+            fl_1 = method(method(:,FLOODINGID) == flIds(ff) & method(:,SEED) == seeds(ss) & (method(:,SRCNODE) == src_nodes(sn)),:);
+
+            %size(fl_1)
+            %fl_1(:,[RXNODE TXNODE SRCNODE ])
+            src_node = unique(fl_1(:,SRCNODE));
+            assert(size(src_node,1) == 1 & size(src_node,2) == 1);
+
+            start_time = min(fl_1(fl_1(:,RXNODE) == src_node,TIME));
+
+            rx_times = fl_1(fl_1(:,RXNODE) ~= src_node, [RXNODE TIME]);
+            %size(rx_times)
+            rx_nodes = unique(rx_times(:,1));
+            reach = [reach; size(rx_nodes,1)];
+
+            delays = rx_times(:,2) - start_time;
+            all_delays = [all_delays; delays];
+
+            fl_1_sent_src = unique(fl_1(fl_1(:,SRCNODE) == fl_1(:,RXNODE),NOSENT));
+            fl_1_sent_fwd = fl_1(fl_1(:,SRCNODE) == fl_1(:,TXNODE),NOSENT);
+
+            %size(fl_1_sent_src)
+            %size(fl_1_sent_fwd)
+
+            all_sent = [all_sent; sum(fl_1_sent_fwd)];
+        end
+      end
+    end
+
+    size(all_sent)
+
+    reach = reach + 1; % include src
+    reach = reach / size(unique(method(:,RXNODE)),1);
+    all_delays = all_delays * 1e3;
+
+    mpr_delays = [mpr_delays; [all_delays zeros(size(all_delays,1),1) + param_id]];
+    mpr_reach = [mpr_reach; [reach zeros(size(reach,1),1) + param_id]];
+    mpr_sent = [mpr_sent; [all_sent zeros(size(all_sent,1),1) + param_id]];
+    param_id = param_id + 1;
+  end
+
+  mpr_param_id = param_id;
+end
+
 %%
 % PROPOSED METHOD
 if (~exist('prop_sent'))
 
-  param_id = e2e_param_id;
+  param_id = mpr_param_id;
 
   data=load(prop_filename);
 
@@ -488,7 +496,6 @@ if (~exist('prop_sent'))
 
 end
 
-
 if (~exist('nonodes'))
   nonodes = length(unique(fl_1(:,RXNODE)));
   load_data = 0;
@@ -500,18 +507,16 @@ end
 
 %%
 % plot
-%h1 = figure(1);
-h1=figure('Position', [100 100 700 500]);
 
-xlb=[ flood_xlb; mpr_xlb; prob_xlb; e2e_xlb; prop_xlb];
+xlb=[ flood_xlb; prob_xlb; e2e_xlb; mpr_xlb; prop_xlb];
 xlbs = cell(size(xlb,1),1);
 for ii=1:size(xlb,1)
     xlbs{ii} = [int2str(xlb(ii,1)), '/', int2str(xlb(ii,2)), '/',int2str(xlb(ii,3)) ];
 end
 
-delays = [flood_delays; mpr_delays; prob_delays; flood_e2e_delays; prop_delays];
-reach = [flood_reach; mpr_reach; prob_reach; flood_e2e_reach; prop_reach];
-sent = [flood_sent; mpr_sent; prob_sent; flood_e2e_sent; prop_sent];
+delays = [flood_delays; prob_delays; flood_e2e_delays; mpr_delays; prop_delays];
+reach = [flood_reach; prob_reach; flood_e2e_reach; mpr_reach; prop_reach];
+sent = [flood_sent; prob_sent; flood_e2e_sent; mpr_sent; prop_sent];
 
 efficiency = sent;
 efficiency(:,1) = efficiency(:,1)./(reach(:,1)*nonodes); % mac-tx per node, which recv the flood
@@ -544,104 +549,57 @@ end
 dmap = ismember(delays(:,2), idx);
 
 % change
-xlbs{1} ='Baseline';
+xlbs{1} ='naive';
 % NET FL
-xlbs{2} ='1/0';
-xlbs{3} ='2/0';
-xlbs{4} ='3/0';
-xlbs{5} ='4/0';
+xlbs{2} ='1';
+xlbs{3} ='2';
+xlbs{4} ='3';
+xlbs{5} ='4';
 
-% MPR FL
-xlbs{6} ='0/0';
-xlbs{7} ='1/0';
-xlbs{8} ='2/0';
-xlbs{9} ='3/0';
-
-% MPR FL
-xlbs{10} ='0/0';
-xlbs{11} ='1/0';
-xlbs{12} ='2/0';
-xlbs{13} ='3/0';
-xlbs{14} ='0/0';
-xlbs{15} ='1/0';
-xlbs{16} ='2/0';
-xlbs{17} ='3/0';
+% probabilistic FL
+xlbs{6} ='0';
+xlbs{7} ='1';
+xlbs{8} ='2';
+xlbs{9} ='3';
+xlbs{10} ='0';
+xlbs{11} ='1';
+xlbs{12} ='2';
+xlbs{13} ='3';
 
 % E2E FL
-xlbs{18} ='0/1';
-xlbs{19} ='0/2';
-xlbs{20} ='0/3';
-xlbs{21} ='0/4';
+xlbs{14} ='1';
+xlbs{15} ='2';
+xlbs{16} ='3';
+xlbs{17} ='4';
+
+% MPR FL
+xlbs{18} ='0';
+xlbs{19} ='1';
+xlbs{20} ='2';
+xlbs{21} ='3';
+
+for ii=1:21
+%    xlbs{ii} = '';
+end
 
 % Proposed FL
 for ii=22:length(xlbs)
-   xlbs{ii} = strrep(xlbs{ii}, '/0', ''); 
+   xlbs{ii} = [' ', strrep(xlbs{ii}, '/0', '')]; 
 end
 
-% Create line
-%annotation(h1,'line',[0.458571428571429 0.458571428571429],...
-%    [0.91 0.022],'LineStyle','--');
-
-line_hpos = [0.92 0.102];
-%    [0.201428571428571 0.00899999999999999 0.225714285714286 0.056],...
-tbox1 = [0.3 0.00899999999999999 0.225714285714286 0.056];
-%    [0.518571428571427 0.00899999999999999 0.42 0.056],...
-tbox2 = [0.614 0.00899999999999999 0.42 0.056];
-
-
-%subplot(1,3,1);
-boxplot(delays(dmap,1), delays(dmap,2), 'labels', xlbs(idx));
-%xticklabel_rotate([1:5],45,xlbs(idx),'interpreter','none')
-
-%%
-%get the text labels
-textobjs=findobj(gca,'type','text');
-%rotate the text
-set(textobjs,'rotation',45);
-%change the font size
-set(textobjs,'fontsize',10);
-
-%change the position of the text
-offset_amount = [0 -10 0];
-temptextpositions = get(textobjs,'position');
-for n = 1 : length(textobjs)
-    set(textobjs(n),'position',get(textobjs(n),'position')+offset_amount);
+xlbs2 = {};
+for ii=1:length(xlbs)
+   xlbs2{ii} = '';
 end
 
-%this makes it so that the labels will stay in the same relative position
-%on the figure, scaling when the figure is resized
-set(textobjs,'units','data');
-
-%code from source 1. This does something conceptually equivalent to 
-%removing the link that the text has to the boxplot, creating a copy of the
-%text, and deleting the original text. (correct me if I'm wrong).
-copyobj(textobjs,gca);
-delete(textobjs);
-
-% Create textbox
-annotation(h1,'textbox',...
-    tbox1,...
-    'String',{'#NET/#E2E repetitions'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create textbox
-annotation(h1,'textbox',...
-    tbox2,...
-    'String',{'Proposed (max. #MAC/#NET retries)'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-%naive vs mpr
-annotation(h1,'line',[0.248 0.248],line_hpos,'LineStyle','--');
-%mpr vs prob
-annotation(h1,'line',[0.342 0.342],line_hpos,'LineStyle','--');
-%prob vs e2e
-annotation(h1,'line',[0.529 0.529],line_hpos,'LineStyle','--');
-%e2e vs prop
-annotation(h1,'line',[0.624 0.624],line_hpos,'LineStyle','--');
+dir = 'res/';
 
 %%
+h1=figure('Position', [100 100 700 500]);
+boxplot(delays(dmap,1), delays(dmap,2), 'labels', xlbs2(idx));
+flooding_caption(h1, xlbs(idx));
+set(h1, 'Position', [100 100 1000 300]);
+
 title('Delay');
 ylabel('Delay in [ms]');
 %xlabel('Flooding Schemes');%MAC/NET/E2E Retries');
@@ -649,69 +607,16 @@ grid on;
 hold on
 plot(d_men_val(idx), 'gd', 'MarkerFaceColor','green');
 
-saveas(h1, 'delay.eps' ,'epsc');
-
+exportfig(gcf, [dir, 'delay', suffix ,'.eps'], 'LineStyleMap', [], 'Color', 'rgb');
 rmap = ismember(reach(:,2), idx);
 
 %subplot(1,3,2);
-%h2=figure(2);
+%%
 h2=figure('Position', [100 100 700 500]);
-boxplot(reach(rmap,1), reach(rmap,2), 'labels', xlbs(idx));
+boxplot(reach(rmap,1), reach(rmap,2), 'labels', xlbs2(idx));
+flooding_caption(h2, xlbs(idx));
+set(h2, 'Position', [100 100 1000 300]);
 
-%%
-%get the text labels
-textobjs=findobj(gca,'type','text');
-%rotate the text
-set(textobjs,'rotation',45);
-%change the font size
-set(textobjs,'fontsize',10);
-
-%change the position of the text
-offset_amount = [0 -10 0];
-temptextpositions = get(textobjs,'position');
-for n = 1 : length(textobjs)
-    set(textobjs(n),'position',get(textobjs(n),'position')+offset_amount);
-end
-
-%this makes it so that the labels will stay in the same relative position
-%on the figure, scaling when the figure is resized
-set(textobjs,'units','data');
-
-%code from source 1. This does something conceptually equivalent to 
-%removing the link that the text has to the boxplot, creating a copy of the
-%text, and deleting the original text. (correct me if I'm wrong).
-copyobj(textobjs,gca);
-delete(textobjs);
-
-% Create textbox
-annotation(h2,'textbox',...
-    tbox1,...
-    'String',{'#NET/#E2E repetitions'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create textbox
-annotation(h2,'textbox',...
-    tbox2,...
-    'String',{'Proposed (max. #MAC/#NET retries)'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create line
-%annotation(h2,'line',[0.458571428571429 0.458571428571429],...
-%    [0.91 0.022],'LineStyle','--');
-
-
-%naive vs mpr
-annotation(h2,'line',[0.248 0.248],line_hpos,'LineStyle','--');
-%mpr vs prob
-annotation(h2,'line',[0.342 0.342],line_hpos,'LineStyle','--');
-%prob vs e2e
-annotation(h2,'line',[0.529 0.529],line_hpos,'LineStyle','--');
-%e2e vs prop
-annotation(h2,'line',[0.624 0.624],line_hpos,'LineStyle','--');
-
-%%
 title('Reachbility');
 ylabel('Reachbility in [%]');
 %xlabel('Flooding Schemes');
@@ -719,64 +624,15 @@ grid on;
 hold on
 plot(r_men_val(idx), 'gd', 'MarkerFaceColor','green');
 
-saveas(h2, 'reach.eps' ,'epsc');
+exportfig(gcf, [dir, 'reach', suffix ,'.eps'], 'LineStyleMap', [], 'Color', 'rgb');
 
 smap = ismember(sent(:,2), idx);
 
-%subplot(1,3,3);
-%h3=figure(3);
-h3=figure('Position', [100 100 700 500]);
-boxplot(sent(smap,1), sent(smap,2), 'labels', xlbs(idx));
 %%
-%get the text labels
-textobjs=findobj(gca,'type','text');
-%rotate the text
-set(textobjs,'rotation',45);
-%change the font size
-set(textobjs,'fontsize',10);
-
-%change the position of the text
-offset_amount = [0 -10 0];
-temptextpositions = get(textobjs,'position');
-for n = 1 : length(textobjs)
-    set(textobjs(n),'position',get(textobjs(n),'position')+offset_amount);
-end
-
-%this makes it so that the labels will stay in the same relative position
-%on the figure, scaling when the figure is resized
-set(textobjs,'units','data');
-
-%code from source 1. This does something conceptually equivalent to 
-%removing the link that the text has to the boxplot, creating a copy of the
-%text, and deleting the original text. (correct me if I'm wrong).
-copyobj(textobjs,gca);
-delete(textobjs);
-
-% Create textbox
-annotation(h3,'textbox',...
-    tbox1,...
-    'String',{'#NET/#E2E repetitions'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create textbox
-annotation(h3,'textbox',...
-    tbox2,...
-    'String',{'Proposed (max. #MAC/#NET retries)'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create line
-%annotation(h3,'line',[0.458571428571429 0.458571428571429],...
-%    [0.91 0.022],'LineStyle','--');
-%naive vs mpr
-annotation(h3,'line',[0.248 0.248],line_hpos,'LineStyle','--');
-%mpr vs prob
-annotation(h3,'line',[0.342 0.342],line_hpos,'LineStyle','--');
-%prob vs e2e
-annotation(h3,'line',[0.529 0.529],line_hpos,'LineStyle','--');
-%e2e vs prop
-annotation(h3,'line',[0.624 0.624],line_hpos,'LineStyle','--');
+h3=figure('Position', [100 100 700 500]);
+boxplot(sent(smap,1), sent(smap,2), 'labels', xlbs2(idx));
+flooding_caption(h3, xlbs(idx));
+set(h3, 'Position', [100 100 1000 300]);
 
 %%
 title('Efficiency');
@@ -786,73 +642,22 @@ grid on;
 hold on
 plot(s_men_val(idx), 'gd', 'MarkerFaceColor','green');
 
-saveas(h3, 'sent.eps' ,'epsc');
+exportfig(gcf, [dir, 'sent', suffix ,'.eps'], 'LineStyleMap', [], 'Color', 'rgb');
 
 emap = ismember(efficiency(:,2), idx);
 
-%subplot(1,3,3);
-%h3=figure(3);
-h4=figure('Position', [100 100 700 500]);
-boxplot(efficiency(emap,1), efficiency(emap,2), 'labels', xlbs(idx));
 %%
-%get the text labels
-textobjs=findobj(gca,'type','text');
-%rotate the text
-set(textobjs,'rotation',45);
-%change the font size
-set(textobjs,'fontsize',10);
-
-%change the position of the text
-offset_amount = [0 -10 0];
-temptextpositions = get(textobjs,'position');
-for n = 1 : length(textobjs)
-    set(textobjs(n),'position',get(textobjs(n),'position')+offset_amount);
-end
-
-%this makes it so that the labels will stay in the same relative position
-%on the figure, scaling when the figure is resized
-set(textobjs,'units','data');
-
-%code from source 1. This does something conceptually equivalent to 
-%removing the link that the text has to the boxplot, creating a copy of the
-%text, and deleting the original text. (correct me if I'm wrong).
-copyobj(textobjs,gca);
-delete(textobjs);
-
-% Create textbox
-annotation(h4,'textbox',...
-    tbox1,...
-    'String',{'#NET/#E2E repetitions'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create textbox
-%    [0.518571428571427 0.00899999999999999 0.42 0.056],...
-annotation(h4,'textbox',...
-    tbox2,...
-    'String',{'Proposed (max. #MAC/#NET retries)'},...
-    'FitBoxToText','off',...
-    'LineStyle','none');
-
-% Create line
-%annotation(h4,'line',[0.458571428571429 0.458571428571429],...
-%    [0.91 0.022],'LineStyle','--');
-%naive vs mpr
-annotation(h4,'line',[0.248 0.248],line_hpos,'LineStyle','--');
-%mpr vs prob
-annotation(h4,'line',[0.342 0.342],line_hpos,'LineStyle','--');
-%prob vs e2e
-annotation(h4,'line',[0.529 0.529],line_hpos,'LineStyle','--');
-%e2e vs prop
-annotation(h4,'line',[0.624 0.624],line_hpos,'LineStyle','--');
-
+h4=figure('Position', [100 100 700 500]);
+boxplot(efficiency(emap,1), efficiency(emap,2), 'labels', xlbs2(idx));
+flooding_caption(h4, xlbs(idx));
+set(h4, 'Position', [100 100 1000 300]);
 
 %%
 title('Efficiency');
-ylabel('Total number of MAC transmissions per reached node');
+ylabel('Total number of MAC TX per reached node');
 %xlabel('Flooding Schemes');
 grid on;
 hold on
 plot(e_men_val(idx), 'gd', 'MarkerFaceColor','green');
 
-saveas(h4, 'efficiency.eps' ,'epsc');
+exportfig(gcf, [dir, 'efficiency', suffix ,'.eps'], 'LineStyleMap', [], 'Color', 'rgb');
