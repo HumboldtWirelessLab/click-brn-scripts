8a8efc7486bb0236dc0d0a2d1e35f205533536fb
Robert Döring <rdoering.info@gmail.com>
2014-09-27 21:52:36 +0200

    Improved relative pathes.

--- 500-experiments/016-topology/common_evaluation/collect_results.sh
+++ 500-experiments/016-topology/common_evaluation/collect_results.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+dir=$(dirname "$0")
+pwd=$(pwd)
+
+SIGN=`echo $dir | cut -b 1`
+
+case "$SIGN" in
+  "/")
+      DIR=$dir
+      ;;
+  ".")
+      DIR=$pwd/$dir
+      ;;
+    *)
+      echo "Error while getting directory"
+      exit -1
+      ;;
+esac
+
+
 i=1
 first=true
 while [ -e "./$i" ]
@@ -7,12 +26,12 @@ do
 	# header
 	if [ "${first}" == true  ]
 	then
-		../../common_evaluation/transponate_csv.sh ${i}/result.csv | awk -F ',' '{ OFS=","; $1=""; $0=substr($0,2); if(NR == 1) print "sample_nr",$0}'
+		$DIR/transponate_csv.sh ${i}/result.csv | awk -F ',' '{ OFS=","; $1=""; $0=substr($0,2); if(NR == 1) print "sample_nr",$0}'
 		first=false
 	fi
 
 	# content
-	../../common_evaluation/transponate_csv.sh ${i}/result.csv | awk -F ',' '{ OFS=","; $1=""; $0=substr($0,2); if(NR != 1) print sample_nr,$0}' sample_nr=${i}
+	$DIR/transponate_csv.sh ${i}/result.csv | awk -F ',' '{ OFS=","; $1=""; $0=substr($0,2); if(NR != 1) print sample_nr,$0}' sample_nr=${i}
 
 
 	i=$((i+1));
