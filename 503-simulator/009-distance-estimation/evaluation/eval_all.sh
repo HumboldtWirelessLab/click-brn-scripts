42c8aec8fcf3a0b6c3704c2fb97f5557688603b3
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2014-12-18 17:30:39 +0100

    dist est: update

--- 503-simulator/009-distance-estimation/evaluation/eval_all.sh
+++ 503-simulator/009-distance-estimation/evaluation/eval_all.sh
@@ -6,10 +6,11 @@ if [ "x$RESULTDIR" = "x" ]; then
   RESULTDIR=$PWD
 fi
 
-ALGSEDARG="-e s#none#0#g -e s#default#1#g-e s#tworayground#2#g -e s#shadowing#3#g -e s#ricean#4#g -e s#nakagami#5#g"
+ALGSEDARG="-e s#none#0#g -e s#default#1#g -e s#tworayground#2#g -e s#shadowing#3#g -e s#ricean#4#g -e s#nakagami#5#g"
 
 echo -n "" > result_dist.dat
 echo -n "" > result.dat
+echo -n "" > result_index.dat
 
 for d in `(cd $RESULTDIR; ls -l | grep "^d" | grep -v "evaluation" | awk '{print $NF}')`; do
   if [ -e $RESULTDIR/$d/params ]; then
@@ -19,9 +20,10 @@ for d in `(cd $RESULTDIR; ls -l | grep "^d" | grep -v "evaluation" | awk '{print
     SIMID=`echo $d | sed -e "s#_# #g" | awk '{print $2}'`
     RADIOID=`echo $VAR_RADIO | sed $ALGSEDARG`
 
-    echo "$SIMID $RADIOID $VAR_RXRANGE $VAR_FADING $VAR_PL_EXP $VAR_STD_DB $VAR_INIT_STD_DB $VAR_RICEAN_MAXV $VAR_RICEAN_K $PACKETS" >> result_dist.dat
+    echo "$SIMID $PACKETS" | sed $ALGSEDARG >> result_dist.dat
 
-    cat $RESULTDIR/$d/1/result.dat | sed $ALGSEDARG >> result.dat
+    cat $RESULTDIR/$d/1/result.dat | awk -v ID=$SIMID '{print $1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "ID}' | sed $ALGSEDARG | sort -u >> result_index.dat
+    cat $RESULTDIR/$d/1/result.dat | awk -v ID=$SIMID '{print $9" "$10" "$11" "$12" "$13" "ID}' >> result.dat
   fi
 
 done
