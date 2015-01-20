569c71b8a7d914a603e96aaf1294a9421d3f033f
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2015-01-20 14:21:58 +0100

    Flooding: evaluation

--- 500-experiments/002-flooding-evaluation/003-compare-flooding/echo_result.sh
+++ 500-experiments/002-flooding-evaluation/003-compare-flooding/echo_result.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in 1_MBit*; do echo -n "$i: ";
-  cat $i/evaluation/flooding_info/floodingstats.csv | awk -F"," '{print $6" "$3}';
+  cat $i/evaluation/flooding_info/floodingstats.mat | awk '{print $6" "$3}';
  # cat $i/flooding.click.eth0 | grep OverlayFlooding;
  # cat $i/evaluation/flow_info/flowtime.mat;
 done
